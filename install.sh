ca7328cd378ae2179bd5e00a17a95b632813044a
east0822@gmail.com <donghokang>
2018-09-17 22:40:04 +0200

    install script minor typo fixed

--- install.sh
+++ install.sh
@@ -18,20 +18,20 @@ sudo apt-get install -y -qq libboost-all-dev
 
 # GNUPLOT
 if [ "$yrelease" -eq "16" ]; then
-    sudo apt-get install $APT_GET_FLAGS gnuplot5
+    sudo apt-get install -y -qq gnuplot5
 else 
     if [ "$yrelease" -eq "14" ]; then
-     cd $RAI_ROOT/dependencies
-     # plotting dependencies
-     sudo apt-get install $APT_GET_FLAGS libqt4-dev
-     sudo apt-get install $APT_GET_FLAGS libcairo2-dev
-     sudo apt-get install $APT_GET_FLAGS libpango1.0-dev
-     wget https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.0.5/gnuplot-5.0.5.tar.gz
-     tar -xvzf gnuplot-5.0.5.tar.gz
-     rm gnuplot-5.0.5.tar.gz
-     cd gnuplot-5.0.5/
-     ./configure --prefix=/usr --disable-wxwidgets && sudo make all -j && sudo make install
-     cd $RAI_ROOT
+        cd $RAI_ROOT/dependencies
+        # plotting dependencies
+        sudo apt-get install -y -qq libqt4-dev
+        sudo apt-get install -y -qq libcairo2-dev
+        sudo apt-get install -y -qq libpango1.0-dev
+        wget https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.0.5/gnuplot-5.0.5.tar.gz
+        tar -xvzf gnuplot-5.0.5.tar.gz
+        rm gnuplot-5.0.5.tar.gz
+        cd gnuplot-5.0.5/
+        ./configure --prefix=/usr --disable-wxwidgets && sudo make all -j && sudo make install
+        cd $RAI_ROOT
     fi
 fi
 
@@ -40,5 +40,6 @@ mkdir build
 cd build
 cmake ..
 sudo make install -j
+sudo rm -rf build 
 cd ..
 
